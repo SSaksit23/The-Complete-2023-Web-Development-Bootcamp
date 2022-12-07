a35f4a1336b29ec9a92af550172439c9f598f6be
Saksit Saelow <86817591+SSaksit23@users.noreply.github.com>
2022-12-07 20:05:20 +0700

    My express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get("/", function(req, res){
+  res.send("Hi, there");
+});
+
+app.listen(3000, function(){
+  console.log("Example port")
+});
